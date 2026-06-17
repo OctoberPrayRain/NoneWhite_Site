6fccf3674fdd6b600ff5fd9231fea00724ffabd6
NoneWhite <biggaybodj@gmail.com>
2026-06-17 09:37:49 +0800

    style(frontend): 更新页脚文件社区文案
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-openagent)
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

--- client/src/components/AppFooter.vue
+++ client/src/components/AppFooter.vue
@@ -1,6 +1,6 @@
 <template>
   <footer class="site-footer">
-    <p>NoneWhite · Galgame 分享交流社区</p>
-    <p>致力于独立游戏分享与交流的玩家社区</p>
+    <p>NoneWhite · 分享交流社区</p>
+    <p>致力于优质资源分享与交流的用户社区</p>
   </footer>
 </template>
